3548b6892497803806db77f8e7625341f6379dbd
Sam Arshad <jsb211@txstate.edu>
2025-02-05 19:24:48 -0600

    Updated Comment for previous commit

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from discord import Interaction
 load_dotenv()
 
 intents = discord.Intents.default       #permission sets to dicord. 
-intents.message_content = True          #messsage contents stuff is true. 
+intents.message_content = True          #messsage contents (read, write messages) stuff is true. 
 TOKEN = os.getenv("discord_token")
 
