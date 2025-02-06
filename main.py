54062fdb1cdd5a5e84ac7e3152ceeff5e33890f1
Sam Arshad <jsb211@txstate.edu>
2025-02-05 19:44:29 -0600

    Got Bot Online - Bot has a weird name.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -7,20 +8,18 @@ from discord import Interaction
 # Load the contents of dot env
 load_dotenv()
 
-intents = discord.Intents.default       #permission sets to dicord. 
+intents = discord.Intents.default()       #permission sets to dicord. 
 intents.message_content = True          #messsage contents (read, write messages) stuff is true. 
-TOKEN = os.getenv("discord_token")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 
 # Assign a function when certain commands happen
-client = commands.Bot(command_prefix= "?", intents=intents)
+client = commands.Bot(command_prefix="?", intents=intents)
 
 
 @client.event
 async def on_ready():
     print(f"{client.user.name} is ready!")
-
-
 
 client.run(token=TOKEN)
 
