97f515649809f9848021a22513fb00e4346ec04f
Sam Arshad <jsb211@txstate.edu>
2025-02-05 19:21:18 -0600

    Added newer imports, loaded from env and made first variable.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import os
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+from discord import Interaction
+
+# Load the contents of dot env
+load_dotenv()
+
+intents = discord.Intents.default
